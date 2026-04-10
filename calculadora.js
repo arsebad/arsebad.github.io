edf8cf4523d0170b311047725d6247806ad627b1
Arsebad <137964740+arsebad@users.noreply.github.com>
2026-04-10 16:03:25 -0500

    Actualizar calculadora.js
    - Se cambio fuerza de levantamiento a fuerza de golpeo

--- calculadora.js
+++ calculadora.js
@@ -86,8 +86,8 @@ document.getElementById("formFisico").addEventListener("submit", function(e){
         factor * calculaReflejos(PDE, altura, peso)
     );
 
-    let fuerza = Math.round(
-        factor * calculaFuerza(edad, peso, altura, PDE)
+    let fuerza = (Math.round(
+        factor * calculaFuerza(edad, peso, altura, PDE))/2
     );
 
     let energia = Math.round(
